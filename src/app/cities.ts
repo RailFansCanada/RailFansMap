df1626b4d17bbc09353c50bef537a08f20d4f342
Derek Ellis <derek@llamabagel.ca>
2021-03-30 22:23:49 -0400

    Fix data issues
    * Added Greenboro platform
    * Added missing elevated track segments at Lincoln Fields
    * Re-ordered Lines 1 and 3 so 3 is shown above 1
    * Added future storage tracks at Algonquin
    * Fixed missing overpass at Hunt Club for Line 4

--- src/app/cities.ts
+++ src/app/cities.ts
@@ -6,21 +6,22 @@ export const cities = {
       "ottawa/greenbankYard.json",
       "ottawa/moodieYard.json",
       "ottawa/walkleyYard.json",
-      "ottawa/line3.json",
       "ottawa/line1.json",
+      "ottawa/line3.json",
       "ottawa/line4.json",
       "ottawa/line2.json",
       "ottawa/stage3barrhaven.json",
       "ottawa/stage3kanata.json",
     ],
   },
-  toronto: {
-    location: [-79.3848, 43.65354],
-    data: [
-      "toronto/line1.json",
-      "toronto/line2.json",
-      "toronto/line3.json",
-      "toronto/line4.json"
-    ]
-  }
+  // TODO: Enable Toronto data
+  // toronto: {
+  //   location: [-79.3848, 43.65354],
+  //   data: [
+  //     "toronto/line1.json",
+  //     "toronto/line2.json",
+  //     "toronto/line3.json",
+  //     "toronto/line4.json"
+  //   ]
+  // }
 };
