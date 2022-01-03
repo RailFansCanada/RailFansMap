7db0c09c77b27aa6e6c2cd235d198de832dc95dd
Derek Ellis <derek@llamabagel.ca>
2022-01-03 17:56:27 -0500

    Git add CONTRIBUTING.md and HOW_TO.md (#69)

---
+++ CONTRIBUTING.md
@@ -0,0 +1,41 @@
+# Contributing
+
+There are many ways to contribute to the rail map, including adding new data, reporting data errors or bugs, or adding new features. All contributions are welcome.
+
+There are a number of [how-to guides](/HOW_TO.md) available on how to complete specific tasks when adding/modifying data and features.
+
+## Reporting an Error or Bug
+
+Please open an issue on this repository to report the problem. Include as many details as possible so that the problem can be identified.
+If you are able to, please feel free to try and fix the problem yourself.
+
+If you do not have a GitHub account, errors and bugs can also be reported to `map@railfans.ca`.
+
+## Development Environment Setup
+
+Compiling the map requires Node.js installed. Dependencies are managed with yarn.
+
+Build and run the app locally:
+```bash
+$ yarn dev
+```
+
+This will build the map app and start a web server to use the app. Typically the server is available at `http://localhost:8080` after building.
+
+## Running Tests
+
+Tests are used to validate data to ensure that all properties are specified correctly. To run the tests:
+
+```bash
+$ yarn test
+```
+
+## FAQ
+
+* **Q: Can I add fantasy lines to the map?**  
+A: We strive to be as accurate as possible and therefore we only include data for lines that exist, are under-construction, or are being officially studied. You are welcome to fork this project and create your own maps however.
+
+* **Q: Can I add data for a new region to the map?**  
+A: We are currently focusing on urban rail systems in Canada and there are many that still need to be added. We will happily accept contributions for these regions. 
+
+If you have any additional questions, feel free to open an issue or start a discussion on this repository.
--- utils/bbox.js
+++ utils/bbox.js
@@ -1,15 +1,61 @@
 const fs = require("fs");
 const bbox = require("@turf/bbox").default;
+const bboxPolygon = require("@turf/bbox-polygon").default;
 
 const config = require("../src/config.json");
 
-Object.values(config.agencies).flatMap((it) => it.data).forEach(file => {
-    const content = fs.readFileSync(`./data/${file}`, { encoding: "utf-8" });
-    /** @type {import('geojson').FeatureCollection} */
-    const feature = JSON.parse(content);
+function formatBBox(bbox) {
+  return JSON.stringify(bbox).replaceAll(/,/g, ", ");
+}
 
-    const featureBBox = bbox(feature);
-    feature.bbox = featureBBox;
+if (process.argv.length < 3) {
+  console.error("Usage: node bbox.js <filename>");
+  exit();
+}
 
-    fs.writeFileSync(`./data/${file}`, JSON.stringify(feature));
-});
+const filePath = process.argv[2];
+
+if (filePath === "config.json" || filePath === "config") {
+  console.log("Agencies:");
+  const agencyBBoxes = {};
+  config.agencies.forEach((agency) => {
+    const allFeatures = [];
+    agency.data.forEach((file) => {
+      const content = fs.readFileSync(`./data/${file}`, { encoding: "utf-8" });
+      /** @type {import('geojson').FeatureCollection} */
+      const feature = JSON.parse(content);
+
+      allFeatures.push(feature.features);
+    });
+
+    const featureBBox = bbox({
+      type: "FeatureCollection",
+      features: allFeatures.flat(),
+    });
+    agencyBBoxes[agency.id] = featureBBox;
+    console.log(`    ${agency.id}: ${formatBBox(featureBBox)}`);
+  });
+
+  console.log("Regions:");
+  config.regions.forEach((region) => {
+    const regionFeatures = [];
+    region.agencies.forEach((agency) => {
+      const polygon = bboxPolygon(agencyBBoxes[agency]);
+      regionFeatures.push(polygon);
+    });
+
+    const featureBBox = bbox({
+      type: "FeatureCollection",
+      features: regionFeatures
+    });
+    console.log(`    ${region.id}: ${formatBBox(featureBBox)}`);
+  });
+} else {
+  const content = fs.readFileSync(`./data/${filePath}`, { encoding: "utf-8" });
+  /** @type {import('geojson').FeatureCollection} */
+  const feature = JSON.parse(content);
+
+  const featureBBox = bbox(feature);
+
+  console.log(`bbox for ${filePath}: ${formatBBox(featureBBox)}`);
+}
