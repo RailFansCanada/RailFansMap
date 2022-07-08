9ee007c1c7a831ca19104e4b1e57f968275d18cf
Derek Ellis <derekellis170@gmail.com>
2022-07-07 22:32:41 -0400

    Fix bounding boxes
    Add tests for region bounding boxes

--- tests/bbox.test.js
+++ tests/bbox.test.js
@@ -5,6 +5,23 @@ const booleanContains = require("@turf/boolean-contains").default;
 const bboxPolygon = require("@turf/bbox-polygon").default;
 const bbox = require("@turf/bbox").default;
 
+function getAgencyBboxCollection(agency) {
+  const bboxes = agency.data.map((file) => {
+    const content = fs.readFileSync(`./data/${file}`, { encoding: "utf-8" });
+
+    /** @type {import('geojson').FeatureCollection} */
+    const feature = JSON.parse(content);
+    return bbox(feature);
+  });
+
+  /** @type {import('geojson').FeatureCollection} */
+  const agencyCollection = {
+    type: "FeatureCollection",
+    features: bboxes.map((bbox) => bboxPolygon(bbox)),
+  };
+  return agencyCollection;
+}
+
 expect.extend({
   bboxContains(received, bbox) {
     const receivedPolygon = bboxPolygon(received);
@@ -31,28 +48,16 @@ expect.extend({
   },
 });
 
-test.each(Object.values(config.agencies))(
+test.each(config.agencies)(
   "validate bbox for $name",
   (value) => {
-    const bboxes = value.data.map((file) => {
-      const content = fs.readFileSync(`./data/${file}`, { encoding: "utf-8" });
-
-      /** @type {import('geojson').FeatureCollection} */
-      const feature = JSON.parse(content);
-      return bbox(feature);
-    });
-
-    /** @type {import('geojson').FeatureCollection} */
-    const agencyCollection = {
-      type: "FeatureCollection",
-      features: bboxes.map((bbox) => bboxPolygon(bbox)),
-    };
+    const agencyCollection = getAgencyBboxCollection(value);
 
     expect(value.bbox).bboxContains(bbox(agencyCollection));
   }
 );
 
-test.each(Object.values(config.agencies).flatMap((it) => it.data))(
+test.each(config.agencies.flatMap((it) => it.data))(
   "validate individual bbox for %s",
   (value) => {
     const content = fs.readFileSync(`./data/${value}`, { encoding: "utf-8" });
@@ -61,5 +66,22 @@ test.each(Object.values(config.agencies).flatMap((it) => it.data))(
 
     expect(feature).hasBBox();
     expect(feature.bbox).bboxContains(bbox(feature));
+  }
+);
+
+test.each(Object.values(config.regions))(
+  "validate bbox for region $title",
+  (value) => {
+    const features = value.agencies.flatMap((agency) => {
+      return getAgencyBboxCollection(config.agencies.find((a) => a.id === agency)).features;
+    });
+
+    /** @type {import('geojson').FeatureCollection} */
+    const collection = {
+      "type": "FeatureCollection",
+      features
+    }
+
+    expect(value.bbox).bboxContains(bbox(collection));
   }
 );
