f2fb13d18a5404f6ab251445fcbdd58d3682128f
Derek Ellis <derekellis170@gmail.com>
2022-10-30 23:06:43 -0400

    Remove useMapTarget changes

--- src/hooks/useMapTarget.tsx
+++ src/hooks/useMapTarget.tsx
@@ -5,24 +5,10 @@ export type SimpleBBox = [number, number, number, number];
 type MapTarget = {
   target?: SimpleBBox;
   setTarget: (target: SimpleBBox) => void;
-
-  currentZoom?: number;
-  targetZoom?: number;
-  setCurrentZoom: (zoom: number) => void;
-  setTargetZoom: (zoom: number) => void;
-
-  currentBearing?: number;
-  targetBearing?: number;
-  setCurrentBearing: (bearing: number) => void;
-  setTargetBearing: (bearing: number) => void;
 };
 
 const useProvideMapTarget = (): MapTarget => {
   const [target, setTarget] = useState<SimpleBBox>();
-  const [currentZoom, setCurrentZoom] = useState<number>();
-  const [targetZoom, setTargetZoom] = useState<number>();
-  const [currentBearing, setCurrentBearing] = useState<number>();
-  const [targetBearing, setTargetBearing] = useState<number>();
 
   const updateTarget = (newTarget: SimpleBBox) => {
     setTarget(newTarget);
@@ -31,14 +17,6 @@ const useProvideMapTarget = (): MapTarget => {
   return {
     target,
     setTarget: updateTarget,
-    currentZoom,
-    targetZoom,
-    setCurrentZoom,
-    setTargetZoom,
-    currentBearing,
-    targetBearing,
-    setCurrentBearing,
-    setTargetBearing
   };
 };
 
